5a0b438cd4dbbbec68d69baa43498babc840b531
Rami <138556019+rami242424@users.noreply.github.com>
2024-05-09 21:45:03 +0900

    수정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,7 @@
 
 import styles from "../styles/home.module.css";
 import Movie from "../../components/movie";
-import { API_URL } from "../constants";
+import { API_URL } from './../constants';
 
 export const metadata = {
     title : "Home",
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,16 +3,12 @@ import styles from "../app/styles/movie-videos.module.css";
 import { API_URL } from './../app/constants';
 
 async function getVideos(id: string){
-    // console.log(`Fetching videos: ${Date.now()}`);
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-    // throw new Error('something broke...')
     const response = await fetch(`${API_URL}/${id}/videos`);
     return response.json();
 }
 
 export default async function MovieVideos({id} : {id:string}){
     const videos = await getVideos(id);
-    // return <h6>{JSON.stringify(videos)}</h6>
     return (
         <div className={styles.container}>
             {videos.map(video => (
