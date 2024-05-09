e0b4977e114e6436931d76b421e2e04b9f2f7fa0
Rami <138556019+rami242424@users.noreply.github.com>
2024-05-09 21:18:44 +0900

    #4.4 Dynamic Metadata

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,12 +2,20 @@ import { Suspense } from "react";
 import { API_URL } from "../../../(home)/page";
 import MovieInfo from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+import { getMovie } from "../../../../components/movie-info";
 
-export default async function MovieDetail({
-    params:{id},
-} : {
+// API로 부터 제목 가져오기
+interface IParams {
     params: {id: string};
-}) {
+}
+export async function generateMetadata({params:{id}} : IParams){
+    const movie = await getMovie(id);
+    return {
+        title: movie.title,
+    };
+}
+
+export default async function MovieDetailPage({params:{id}} : IParams){
     return (
         <div>
             <Suspense fallback={<h1>Loading movie info</h1>}>
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -2,8 +2,7 @@
 import styles from "../app/styles/movie-info.module.css";
 import { API_URL } from "../app/(home)/page";
 
-async function getMovie(id:string){
-
+export async function getMovie(id:string){
     const response = await fetch(`${API_URL}/${id}`);
     return response.json();
 }
@@ -15,7 +14,7 @@ export default async function MovieInfo({id} : {id:string}){
             <img src={movie.poster_path} className={styles.poster} alt={movie.title}/>
             <div className={styles.info}>
                 <h1 className={styles.title}>{movie.title}</h1>
-                <h3>⭐️{movie.vote_average.toFixed(1)}</h3>
+                <h3>⭐️{movie.vote_average.toFixed()}</h3>
                 <p>{movie.overview}</p>
                 <a href={movie.homepage} target={"_blank"}>Homepage &rarr;</a>
             </div>
